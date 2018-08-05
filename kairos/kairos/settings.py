ec80e645a4b5c3f6a25dac4357256ba1b61a21d2
Leonardo <leonardo.bilar@gmail.com>
2018-08-05 20:53:28 -0300

    Configuração de autorização de hosts.

--- kairos/kairos/settings.py
+++ kairos/kairos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's3j=o)cjn@m8_!iyn)#+yc#^!73c#h+!20op&_@pk7kih9s%p_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
