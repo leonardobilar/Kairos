084841abd466eb68c04e4b9c90cd5f81c6aefbc0
Leonardo <leonardo.bilar@gmail.com>
2018-08-18 18:01:53 -0300

    Criação do redirecionamento para a view de cadastro de usuário.

--- kairos/cadastro/views.py
+++ kairos/cadastro/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Usuario
+
+class IndexView(generic.ListView):
+    template_name = 'cadastro/index.html'
+    context_object_name = 'usuario'
+
+    def get_queryset(self):
+        return Usuario.objects
