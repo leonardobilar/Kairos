6e13f22dedd92d6d32faf54c0a5f62fa25bec901
Leonardo <leonardo.bilar@gmail.com>
2018-08-25 13:57:56 -0300

    Adicionado os modelos do cadastro de endereço de Entrega e Cobrança.
    Ajuste no template do cadastro de usuário.
    
    Adicionada a validação de endereço de email válido.

--- kairos/cadastro/models.py
+++ kairos/cadastro/models.py
@@ -6,7 +6,7 @@ class Usuario(models.Model):
     sobrenome = models.CharField(max_length = 30)
     email = models.EmailField(unique = True)
     login = models.CharField(max_length = 30, unique = True)
-    senha = models.CharField(max_length = 255)
+    senha = models.EmailField()
     criacao = models.DateTimeField(auto_now = False, auto_now_add = True)
     ativo = models.BooleanField(default = False)
 
@@ -29,6 +29,22 @@ class Vendedor(models.Model):
 class Contato(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
     tipo = models.CharField(max_length = 30)
-    pais = models.IntegerField(max_length = 3)
+    pais = models.IntegerField()
     codigo_regiao = models.IntegerField(max_length = 2)
-    numero = models.IntegerField(max_length = 9)
+    numero = models.IntegerField()
+
+class EnderecoEntrega(models.Model):
+    cep = models.IntegerField()
+    logradouro = models.CharField(max_length = 20)
+    endereco = models.CharField(max_length = 255)
+    numero = models.CharField(max_length = 10)
+    complemento = models.CharField(max_length = 50)
+    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+
+class EnderecoCobranca(models.Model):
+    cep = models.IntegerField()
+    logradouro = models.CharField(max_length = 20)
+    endereco = models.CharField(max_length = 255)
+    numero = models.CharField(max_length = 10)
+    complemento = models.CharField(max_length = 50)
+    usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
